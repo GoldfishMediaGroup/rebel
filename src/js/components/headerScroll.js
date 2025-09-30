011e7d41500a6bff1d1b7381c5eb96cc9a1c75fa
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2025-10-01 00:34:43 +0400

    add header, footer, cookie

---
+++ src/js/components/headerScroll.js
@@ -0,0 +1,33 @@
+
+function headerScroll () {
+    const nav = document.querySelector('header');
+    const navOffsetTop = nav.offsetTop;
+  
+
+    function handleScroll() {
+      if (window.scrollY > navOffsetTop) {
+        nav.classList.add('isScroll');
+      } else {
+        nav.classList.remove('isScroll');
+      }
+    }
+    
+    function handleResize() {
+      window.addEventListener('scroll', handleScroll);
+    }
+
+    if (nav) {
+      handleResize();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+export default headerScroll
