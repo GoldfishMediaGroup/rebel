5793b12a2ffb2666a86feedb571f64818cfc19fc
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2025-11-14 14:31:11 +0400

    add news det page

---
+++ src/js/components/newsDetHero.js
@@ -0,0 +1,37 @@
+import Swiper from 'swiper/bundle';
+import { rem } from '../utils/constants';
+
+function newsDetHero() {
+  const section = document.querySelector('.news-det-hero');
+
+  if (!section) return;
+
+  const swiper = new Swiper('.news-det-hero__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    speed: 800,
+    spaceBetween: rem(4),
+    navigation: {
+      prevEl: section.querySelector('.swiper-button-prev'),
+      nextEl: section.querySelector('.swiper-button-next')
+    },
+    pagination: {
+      el: section.querySelector('.swiper-pagination')
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 'auto',
+            spaceBetween: rem(2.8),
+      }
+    }
+  });
+
+  const slides = document.querySelectorAll('.news-det-hero__slide');
+  slides.forEach((item) => item.classList.add('isTransition'));
+
+
+  
+}
+
+export default newsDetHero;
