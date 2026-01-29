e350aad6315b91aa4f2dcfd0920af1bad928cef3
O.Kuloverova <oliich23@mail.ru>
2026-01-29 18:46:23 +0400

    add swiper  horisontal section

--- src/js/components/horisontalNew.js
+++ src/js/components/horisontalNew.js
@@ -13,39 +13,39 @@ function horisontalNew() {
     speed: 1000,
     slidesPerView: 1,
     grabCursor: true,
-    mousewheel: false, // ВАЖНО: выключаем, теперь рулит ScrollTrigger
-    keyboard: true
+    // mousewheel: false, // ВАЖНО: выключаем, теперь рулит ScrollTrigger
+    // keyboard: true
   });
 
-  const animTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: section,
-      start: 'top top',
-      pin: true,
-      // Увеличиваем end, чтобы на каждый слайд приходилось достаточно "пути" скролла
-      end: () => `+=${cards.length * 150}%`,
-      scrub: true,
-      anticipatePin: 1,
-      invalidateOnRefresh: true,
-      snap: {
-        snapTo: 1 / (cards.length - 1),
-        duration: { min: 0.2, max: 0.5 },
-        delay: 0,
-        ease: 'power1.inOut'
-      },
-      onUpdate: (self) => {
-        // Вычисляем, какой слайд должен быть активен исходя из прогресса (0...1)
-        const progress = self.progress;
-        const totalSlides = cards.length - 1;
-        const targetIndex = Math.round(progress * totalSlides);
+//   const animTimeline = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: section,
+//       start: 'top top',
+//       pin: true,
+//       // Увеличиваем end, чтобы на каждый слайд приходилось достаточно "пути" скролла
+//       end: () => `+=${cards.length * 150}%`,
+//       scrub: true,
+//       anticipatePin: 1,
+//       invalidateOnRefresh: true,
+//       snap: {
+//         snapTo: 1 / (cards.length - 1),
+//         duration: { min: 0.2, max: 0.5 },
+//         delay: 0,
+//         ease: 'power1.inOut'
+//       },
+//       onUpdate: (self) => {
+//         // Вычисляем, какой слайд должен быть активен исходя из прогресса (0...1)
+//         const progress = self.progress;
+//         const totalSlides = cards.length - 1;
+//         const targetIndex = Math.round(progress * totalSlides);
 
-        // Переключаем Swiper только если индекс реально изменился
-        if (swiper.activeIndex !== targetIndex) {
-          swiper.slideTo(targetIndex);
-        }
-      }
-    }
-  });
+//         // Переключаем Swiper только если индекс реально изменился
+//         if (swiper.activeIndex !== targetIndex) {
+//           swiper.slideTo(targetIndex);
+//         }
+//       }
+//     }
+//   });
 }
 
 export default horisontalNew;
